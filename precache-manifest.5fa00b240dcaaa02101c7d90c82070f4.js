1cc191cb5d077e7e61a28d4d8893a8e1046c4bd0
Matt Kharrl <colbenkharrl@gmail.com>
2019-01-12 14:25:05 -0800

    Updates

---
+++ precache-manifest.5fa00b240dcaaa02101c7d90c82070f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9da131aaf24e2a5d999f",
+    "url": "/tracker/static/js/runtime~main.9da131aa.js"
+  },
+  {
+    "revision": "96f9fd38ff60690674bb",
+    "url": "/tracker/static/js/main.96f9fd38.chunk.js"
+  },
+  {
+    "revision": "eedf4ef4727cb2bc899f",
+    "url": "/tracker/static/js/1.eedf4ef4.chunk.js"
+  },
+  {
+    "revision": "96f9fd38ff60690674bb",
+    "url": "/tracker/static/css/main.839c346b.chunk.css"
+  },
+  {
+    "revision": "d58f783cff63afadaa924f144d22a6ba",
+    "url": "/tracker/index.html"
+  }
+];
